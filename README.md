af1aeb5ba5788b67ae31ee16aadaae11699f8496
malexj <alexmaximovj@gmail.com>
2022-04-15 12:39:17 +0300

    Handle version of app

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# spring-boot-heroku-config-app
+### Spring Cloud Config
+
+#### Configuration:
+
+##### gradle:
+
+* '<b>updateProcfile</b>' task designed to update Procfile file that HEROKU service uses for deployment
+
+#### Documentation links:
+
+* [Spring Cloud Documentation](https://cloud.spring.io/spring-cloud-config/reference/html/)
+* [Simple example](https://betacode.net/11723/understanding-spring-cloud-config-server-with-example)
--- src/main/java/com/malexj/SpringBootHerokuConfigAppApplication.java
+++ src/main/java/com/malexj/SpringBootHerokuConfigAppApplication.java
@@ -4,12 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication
 @EnableConfigServer
+@SpringBootApplication
 public class SpringBootHerokuConfigAppApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootHerokuConfigAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHerokuConfigAppApplication.class, args);
+    }
 
 }
